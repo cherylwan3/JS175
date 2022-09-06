5f3e6509ac71a36bf79426a5ac4c9eeef89ffa11
Cheryl Wan <cherylwan3@gmail.com>
2022-09-05 21:29:59 -0700

    lesson2

--- lesson2/loan_calculator/app_html.js
+++ lesson2/loan_calculator/app_html.js
@@ -2,32 +2,97 @@ const HTTP = require('http');;
 const URL = require('url').URL;
 const PORT = 3000;
 
-const HTML_START = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Loan Calculator</title>
-  </head>
-  <body>
-    <article>
-      <h1>Loan Calculator</h1>
-      <table>
-        <tbody>`;
+function createHTML(APR, amount, duration, monthlyPayment) {
+  const HTML_START = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Loan Calculator</title>
+        <style type="text/css">
+          body {
+            background: rgba(250, 250, 250);
+            font-family: sans-serif;
+            color: rgb(50, 50, 50);
+          }
 
-const HTML_END = `
-        </tbody>
-      </table>
-    </article>
-  </body>
-</html>`;
+          article {
+            width: 100%;
+            max-width: 40rem;
+            margin: 0 auto;
+            padding: 1rem 2rem;
+          }
+
+          h1 {
+            font-size: 2.5rem;
+            text-align: center;
+          }
+
+          table {
+            font-size: 1.5rem;
+          }
+          th {
+            text-align: right;
+          }
+          td {
+            text-align: center;
+          }
+          th,
+          td {
+            padding: 0.5rem;
+          }
+        </style>
+      </head>
+      <body>
+        <article>
+          <h1>Loan Calculator</h1>
+          <table>
+            <tbody>`;
+
+    const HTML_END = `
+            </tbody>
+          </table>
+        </article>
+      </body>
+    </html>`;
+
+  let content = 
+  `<tr>
+    <th>Amount:</th>
+    <td><a href="/?amount=${amount-100}&duration=${duration}">- $100</a></td>
+    <td>$${amount}</td>
+    <td><a href="/?amount=${amount+100}&duration=${duration}">+ $100</a></td>
+  </tr>
+  <tr>
+    <th>Duration:</th>
+    <td><a href="/?amount=${amount}&duration=${duration - 1}">- 1 year</a></td>
+    <td>${duration} year(s)</td>
+    <td><a href="/?amount=${amount}&duration=${duration + 1}">+ 1 year</a></td>
+  </tr>
+  <tr>
+    <th>APR:</th>
+    <td colspan='3'>${APR}%</td>
+  </tr>
+  <tr>
+    <th>Monthly Payment:</th>
+    <td colspan='3'>$${monthlyPayment}</td>
+  </tr>`; 
+  
+  return `${HTML_START}${content}${HTML_END}`
+}
 
 function getParams(path) {
   const myURL = new URL(path, `http://localhost:${PORT}`);
   return myURL.searchParams;
 }
 
+function isInvalid(amount, duration) {
+  return (amount === 0) || (duration === 0);
+}
+
 function calculateMonthlyPayment(APR, amount, duration) {
+  if (isInvalid(amount, duration)) return 0;
+  
   let annualInterestRate = Number(APR) / 100;
   let monthlyInterestRate = annualInterestRate / 12;
   let months = Number(duration) * 12;
@@ -38,19 +103,15 @@ function calculateMonthlyPayment(APR, amount, duration) {
   return monthlyPayment.toFixed(2);
 }
 
+
 function createLoanOffer(params) {
   const APR = 5;
   let amount = Number(params.get('amount'));
   let duration = Number(params.get('duration'));
+ 
   let monthlyPayment = calculateMonthlyPayment(APR, amount, duration);
 
-  let content = `<tr><th>Amount:</th><td>$${amount}</td></tr>
-                 <tr><th>Duration:</th><td>$${duration}</td></tr>
-                 <tr><th>APR:</th><td>${APR}%</td></tr>
-                 <tr><th>Monthly Payment:</th><td>$${monthlyPayment}</td></tr>`;  
-  content = `${HTML_START}${content}${HTML_END}`;
-
-  return content;
+  return createHTML(APR, amount, duration, monthlyPayment);
 }
 
 const SERVER = HTTP.createServer((req, res) => {
