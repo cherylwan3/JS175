4bb0a932af43bf626197596559d56e44bf8f6ecc
Cheryl Wan <cherylwan3@gmail.com>
2022-09-08 12:00:42 -0700

    project

--- lesson4/projects/hello-world/hello-world.js
+++ lesson4/projects/hello-world/hello-world.js
@@ -1,39 +1,39 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
-
-const writeLog = (req, res) => {
-  let timeStamp = String(new Date()).substring(4, 24); // Mmm dd YYYY HH:MM:SS
-  console.log(`${timeStamp} ${req.method} ${req.originalUrl} ${res.statusCode}`);
-};
+app.use(morgan("common"));
 
 app.get("/", (req, res) => {
   res.redirect("/english");
-  writeLog(req, res);
 });
 
 app.get("/english", (req, res) => {
-  res.render("hello-world-english");
-  writeLog(req, res);
+  res.render("hello-world-english", {
+    currentPath: req.path,
+  });
 });
 
 app.get("/french", (req, res) => {
-  res.render("hello-world-french");
-  writeLog(req, res);
+  res.render("hello-world-french", {
+    currentPath: req.path,
+  });
 });
 
 app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian");
-  writeLog(req, res);
+  res.render("hello-world-serbian", {
+    currentPath: req.path,
+  });
 });
 
 app.get("/japanese", (req, res) => {
-  res.render("hello-world-japanese");
-  writeLog(req, res);
+  res.render("hello-world-japanese", {
+    currentPath: req.path,
+  });
 })
 
 app.listen(3000, "localhost", () => {
