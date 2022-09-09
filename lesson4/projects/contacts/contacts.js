bbb1f296049f76bce4e62b0c67fb2ec6dfb06b0a
Cheryl Wan <cherylwan3@gmail.com>
2022-09-09 12:01:17 -0700

    *

--- lesson4/projects/contacts/contacts.js
+++ lesson4/projects/contacts/contacts.js
@@ -45,6 +45,7 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("common"));
 
 app.get("/", (req, res) => {
@@ -57,6 +58,16 @@ app.get("/contacts", (req, res) => {
   });
 });
 
+app.get("/contacts/new", (req, res) => {
+  res.render("new-contact");
+})
+
+app.post("/contacts/new", (req, res) => {
+  contactData.push({ ...req.body });
+  res.redirect("/contacts");
+});
+
 app.listen(3000, "localhost", () => {
   console.log("Listening to port 3000.");
 });
+
