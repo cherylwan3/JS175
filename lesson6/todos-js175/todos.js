ce305b316c17da99a22f286454b9603577f1ec91
Cheryl Wan <cherylwan3@gmail.com>
2022-11-01 14:52:21 -0700

    all

--- lesson6/todos-js175/todos.js
+++ lesson6/todos-js175/todos.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const TodoList = require("./lib/todolist");
 
 const app = express();
 const host = "localhost";
@@ -12,6 +15,15 @@ app.set("view engine", "pug");
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  name: "launch-school-todos-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+}));
+
+app.use(flash());
 
 // return the list of todo lists sorted by completion status and title.
 const sortTodoLists = lists => {
@@ -37,9 +49,47 @@ const sortTodoLists = lists => {
 };
 
 app.get("/", (req, res) => {
+  res.redirect("/lists");
+});
+
+// Render the list of todo lists
+app.get("/lists", (req, res) => {
   res.render("lists", {
     todoLists: sortTodoLists(todoLists),
   });
+});
+
+// Render new todo list page
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+// Create a new todo list
+app.post("/lists", (req, res) => {
+  let title = req.body.todoListTitle.trim();
+
+  if (title.length === 0) {
+    req.flash("error", "A title was not provided.");
+    res.render("new-list", {
+      flash: req.flash(),
+    });
+  } else if (title.length > 100) {
+    req.flash("error", "List title must be between 1 and 100 characters.");
+    res.render("new-list", {
+      flash: req.flash(),
+      todoListTitle: req.body.todoListTitle,
+    });
+  } else if (todoLists.some(list => list.title === title)) {
+    req.flash("error", "List title must be unique.");
+    res.render("new-list", {
+      flash: req.flash(),
+      todoListTitle: req.body.todoListTitle,
+    });
+  } else {
+    todoLists.push(new TodoList(title));
+    req.flash("success", "The todo list has been created.");
+    res.redirect("/lists");
+  }
 });
 
 // Listener
